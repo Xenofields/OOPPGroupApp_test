e47a81ca2250ae40e75dcf62f385a2279a9c7bba
Eugene Tan <eugenetan9134@gmail.com>
2017-12-12 20:20:37 +0800

    updated by Eugene

--- SmartLifestyle(Eugene)/static/js/chores.js
+++ SmartLifestyle(Eugene)/static/js/chores.js
@@ -1,5 +1,31 @@
 window.onload = function(){ 
-	// Get the modal
+	
+	if($('#change1').is(":checked")){
+		$("#auto1").show();
+		$("#manual1").hide();
+		}
+	else{
+		$("#auto1").hide();
+		$('#manual1').show();
+		}	
+	if($('#change2').is(":checked")){
+		$("#auto2").show();
+		$("#manual2").hide();
+		}
+	else{
+		$("#auto2").hide();
+		$('#manual2').show();
+		}	
+	if($('#change3').is(":checked")){
+		$("#auto3").show();
+		$("#manual3").hide();
+	}
+	else{
+		$("#auto3").hide();
+		$('#manual3').show();
+	}
+
+		// Get the modal
 	var modal = document.getElementById('myModal');
 
 	// Get the button that opens the modal
@@ -24,18 +50,43 @@ window.onload = function(){
 		if (event.target == modal) {
 			modal.style.display = "none";
 		}
-	}
-	
-	if($('.change').is(":checked")){
-        $(".te").show();
-		$(".test" ).hide();
-		}
-    else{
-        $(".te").hide();
-		$('.test').show();
-		}
+	}	
+
+
+		
 }	
 
+function addDevice(){
+		var insertpic=document.getElementById("list");
+		var page=document.getElementById('page');
+		var device=document.getElementById('select');
+		div=document.createElement("div");
+		div.className = "device";
+		if (device.value == 'cleaner'){
+			var pic = document.createElement("img");
+			pic.src=('../static/images/cleaner.jpg');
+			pic.className="image"
+			pic.setAttribute('alt','robotic cleaner');
+			div.appendChild(pic);
+			var head = document.createElement("h3");
+			head.innerHTML="Vacuum Cleaner";
+			div.appendChild(head);
+			var p=document.createElement("p");
+			var position=document.getElementById('position')
+			p.innerHTML="Position: " + position.value;
+			div.appendChild(p);
+			var piclist=document.createElement("li");
+			piclist.appendChild(div);
+			insertpic.appendChild(piclist);
+			page.appendChild(insertpic);
+		}
+		else if (device.value == "wash"){
+			
+		}
+		else if (device.value == "mop"){
+			
+		}
+	}
 
 
 
